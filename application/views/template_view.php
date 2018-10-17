e994eedefafbfad70387198f9874898047ec590c
Okki Setyawan <okkisetyawan@gmail.com>
2018-10-18 06:44:21 +0700

    update 18 okt 2017

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -63,6 +63,7 @@
  
     <!-- Jquery DataTable Plugin Js -->
     <script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/dataTables.rowsGroup.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
